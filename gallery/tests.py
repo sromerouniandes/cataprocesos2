9aacdd72762d5884e69792b0e555a387e5b2a55f
Sergio David Romero Maldonado <sromerom@psl.com.co>
2020-03-29 16:53:27 -0500

    Paso 2 estado rojo - Portfolios

--- gallery/tests.py
+++ gallery/tests.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.test import TestCase, Client
 
 # Create your tests here.
-from .models import Image
+from .models import Image, Portfolio
 import json
 
 # Create your tests here.
@@ -42,3 +42,12 @@ class GalleryTestCase(TestCase):
         url = '/gallery/portfolio/'
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, 200)
+
+    def test_count_portfolio_list(self):
+        url = '/gallery/portfolio/'
+        user_model = User.objects.create_user(username='test', password='kd8wke-DE34', first_name='test', last_name='test', email='test@test.com')
+        Portfolio.objects.create(user=user_model);
+        Portfolio.objects.create(user=user_model);
+        response = self.client.get(url, format='json')
+        current_data = json.loads(response.content)
+        self.assertEqual(len(current_data),2)
